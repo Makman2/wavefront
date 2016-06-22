c4a91643a423c95efc036f215e459472252ca9ba
Makman2 <makmanx64@gmail.com>
2016-06-23 00:37:42 +0200

    WIP

---
+++ wavefront/mesh.py
@@ -0,0 +1,6 @@
+# TODO namedtuple?
+
+class Mesh:
+    def __init__(self, name="", vertices=[]):
+        self.name = name
+        self.vertices = vertices
---
+++ wavefront/obj.py
@@ -0,0 +1,64 @@
+import re
+
+from wavefront.mesh import Mesh
+
+
+_anti_comment_regex = re.compile("#.*")
+_token_regex = re.compile("o|v")
+_token_o_regex = re.compile(r"\s+([^\s]+)")
+_token_v_regex = re.compile(r"\s+([0-9]+\.[0-9]+)\s+([0-9]+\.[0-9]+)\s+"
+                            r"([0-9]+\.[0-9]+)(?:\s+([0-9]+\.[0-9]+))?")
+
+
+def load(string):
+    """
+    Yields `Mesh`es from the given wavefront-obj data string.
+
+    :param string: The data string of the obj-file to load.
+    :return:       An iterator returning `Mesh`es loaded from `string`.
+    """
+    # Remove all comment strings.
+    string = _anti_comment_regex.sub("", string)
+
+    pos = 0
+    mesh = None
+    while True:
+        token = _token_regex.search(string, pos)
+
+        if token is None:
+            break
+
+        token_id = token.group()
+
+        if token_id == "o":
+            if mesh:
+                yield mesh
+
+            match = _token_o_regex.match(string, token.endpos)
+            mesh = Mesh(match.group(1))
+
+        elif token_id == "v":
+            match = _token_v_regex.match(string, token.endpos)
+            vertices = tuple(match.group(i) for i in (1, 2, 3))
+            if 4 in match.groups():
+                vertices += (match.group(4),)
+
+            mesh.vertices.append(vertices)
+
+        pos = match.endpos
+
+
+def store(meshes):
+    """
+    Creates a wavefront-obj data-string from the given meshes.
+
+    :param meshes: A sequence of `Mesh`es to store.
+    :return:       The data string of the obj-file.
+    """
+    token_list = []
+    for mesh in meshes:
+        token_list.append("o " + mesh.name)
+        for vector in mesh.vertices:
+            token_list.append("v " + " ".join(vector))
+
+    return "\n".join(token_list)
